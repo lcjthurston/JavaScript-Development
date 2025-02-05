312d80092dc87aaa5a52336bf983ed7c575a2598
Luke Thurston <90153234+lcjthurston@users.noreply.github.com>
2025-02-05 13:31:04 -0600

    Ternary Operator

--- Section 4 - Controlling Program Logic and Flow/03_16_Ternary_Operator/app.js
+++ Section 4 - Controlling Program Logic and Flow/03_16_Ternary_Operator/app.js
@@ -1,3 +1,13 @@
+// ==============
+// OVERVIEW
+// ==============
+// 1. Ternary operator in JS allows us to condense if-else logic into single line of code.
+// 2. Called ternary operator because three arguments, as opposed to binary (2 arguments).
+// 3. Syntax: "condition ? expIfTrue : expIfFalse"
+
+// ==============
+// EXAMPLE 1
+// ==============
 let num = 7;
 
 // Without Ternary Operator...
@@ -15,7 +25,7 @@ num === 7 ? console.log('lucky!') : console.log('BAD!');
 // EXAMPLE 2
 // ==============
 
-let status = 'offline';
+let state = 'offline';
 
 // Without ternary operator
 // let color;
@@ -27,5 +37,5 @@ let status = 'offline';
 // }
 
 // One-liner using Ternary Operator...
-let color = status === 'offline' ? 'red' : 'green';
+let color = state === 'offline' ? 'red' : 'green';
 console.log(color);
