d1586010f23080b66413d2df565230ed8bbc392b
Luke Thurston <90153234+lcjthurston@users.noreply.github.com>
2025-02-05 14:16:08 -0600

    Modules 4.07-4.10

--- Section 5 - Capture Collections of Data with Arrays/04_09_Array_Slice/app.js
+++ Section 5 - Capture Collections of Data with Arrays/04_09_Array_Slice/app.js
@@ -1,5 +1,6 @@
 let animals = [ 'shark', 'salmon', 'whale', 'bear', 'lizard', 'tortoise' ];
 
+// The first arg of slice is the starting index and the second arg is the number of array elements.
 let swimmers = animals.slice(0, 3);
 
 let mammals = animals.slice(2, 4);
@@ -7,7 +8,9 @@ let mammals = animals.slice(2, 4);
 //One option:
 // let reptiles = animals.slice(4, 6);
 
-// Another option:
+// Other options:
+// Go to end of array starting at index 4.
 let reptiles = animals.slice(4);
 
+// Start at end of array and count backwards three elements.
 let quadruped = animals.slice(-3);
